6f5af5d4b59db51cbb7031239c47b2b1292e7475
syota-tominaga <68591369+syota-tominaga@users.noreply.github.com>
2020-08-26 19:15:34 +0900

    ルボコップ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,12 +2,24 @@ class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
 
   def index
+    @items = Item.includes(:user)
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :image, :text, :category_id, :condition_id, :shipping_origin_id, :delivery_fee, :delively_day_id).merge(user_id: current_user.id)
   end
 
   def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
+    redirect_to action: :index unless user_signed_in?
   end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,22 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   has_many :items
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  FULL_WIDTH_REGEX = /\A[ぁ-んァ-ン一-龥]/
-  KATAKANA_REGEX = /\A[ァ-ヶー－]+\z/
-  validates :password, confirmation: true, length: { minimum: 6 }, format: {with: PASSWORD_REGEX, message: "Password confirmation doesn't match Password"}
+  FULL_WIDTH_REGEX = /\A[ぁ-んァ-ン一-龥]/.freeze
+  KATAKANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
+  validates :password, confirmation: true, length: { minimum: 6 }, format: { with: PASSWORD_REGEX, message: "Password confirmation doesn't match Password" }
 
-  #validates :password, confirmation: true, length: { minimum: 6 }, format: { with: /\A[a-zA-Z0-9]+\z/, message: "is invalid. Input half-width characters."}
+  # validates :password, confirmation: true, length: { minimum: 6 }, format: { with: /\A[a-zA-Z0-9]+\z/, message: "is invalid. Input half-width characters."}
 
   with_options presence: true do
     validates :nickname
-    validates :family_name, format: { with: FULL_WIDTH_REGEX, message: "is invalid. Input full-width characters."}
-    validates :first_name, format: { with: FULL_WIDTH_REGEX, message: "is invalid. Input full-width characters."}
-    validates :family_name_kana, format: { with: KATAKANA_REGEX, message: "is invalid. Input full-width katakana characters."}
-    validates :first_name_kana, format: { with: KATAKANA_REGEX, message: "is invalid. Input full-width katakana characters."}
+    validates :family_name, format: { with: FULL_WIDTH_REGEX, message: 'is invalid. Input full-width characters.' }
+    validates :first_name, format: { with: FULL_WIDTH_REGEX, message: 'is invalid. Input full-width characters.' }
+    validates :family_name_kana, format: { with: KATAKANA_REGEX, message: 'is invalid. Input full-width katakana characters.' }
+    validates :first_name_kana, format: { with: KATAKANA_REGEX, message: 'is invalid. Input full-width katakana characters.' }
     validates :birth_day
   end
 end
