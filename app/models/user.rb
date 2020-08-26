b310d5738ee0d36516b654dd286b73d1fb7b3258
syota-tominaga <68591369+syota-tominaga@users.noreply.github.com>
2020-08-26 10:32:26 +0900

    テストコードの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, confirmation: true, length: { minimum: 6 }, format: {with: PASSWORD_REGEX, message: "Password confirmation doesn't match Password"}
+
+  #validates :password, confirmation: true, length: { minimum: 6 }, format: { with: /\A[a-zA-Z0-9]+\z/, message: "is invalid. Input half-width characters."}
 
   with_options presence: true do
     validates :nickname
-    validates :email, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
-    validates :password, confirmation: true, length: { minimum: 6 }, format: { with: /\A[a-zA-Z0-9]+\z/, message: "is invalid. Input half-width characters."}
     validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
     validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
     validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname {"ターロ"}
+    email {"kkk@gmail"}
+    password {"0000aaaa"}
+    password_confirmation {password}
+    family_name {"佐藤"}
+    first_name {"太郎"}
+    family_name_kana {"サトウ"}
+    first_name_kana {"タロウ"}
+    birth_day {"1900/10/10"}
+  end
+end
