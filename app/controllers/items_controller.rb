77fabfbff5532eaf9c2f7f36fba1a758fba59795
syota-tominaga <68591369+syota-tominaga@users.noreply.github.com>
2020-08-27 16:54:10 +0900

    商品出品機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,13 +10,18 @@ class ItemsController < ApplicationController
   end
 
   def create
-    Item.create(item_params)
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render "new"
+    end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:name, :image, :text, :category_id, :condition_id, :shipping_origin_id, :delivery_fee, :delively_day_id).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :text, :category_id, :condition_id, :shipping_origin_id, :delivery_fee_id, :delivery_day_id, :price).merge(user_id: current_user.id)
   end
 
   def move_to_index
