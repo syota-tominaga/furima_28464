b6f255556b57c7ab32681f4df3062650f528039d
syota-tominaga <68591369+syota-tominaga@users.noreply.github.com>
2020-08-28 14:34:22 +0900

    商品出品機能　編集1

--- app/javascript/tax.js
+++ app/javascript/tax.js
@@ -1,11 +1,11 @@
 window.addEventListener("DOMContentLoaded", (function() {
-  var t = location.pathname;
+  const t = location.pathname;
   if ("/items/new" === t || "/items" === t || /^(?=.*item)(?=.*edit)/.test(t)) {
-      var e = document.getElementById("item-price")
+      const e = document.getElementById("item-price")
         , n = document.getElementById("add-tax-price")
         , r = document.getElementById("profit");
       e.addEventListener("input", (function() {
-          var t = document.getElementById("item-price").value;
+          const t = document.getElementById("item-price").value;
           n.innerHTML = Math.floor(.1 * t).toLocaleString(),
           r.innerHTML = Math.floor(.9 * t).toLocaleString()
       }
