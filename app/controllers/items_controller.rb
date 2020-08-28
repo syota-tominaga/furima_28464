13a791a33b3027cf7397ac25b68b5451a6d661cc
syota-tominaga <68591369+syota-tominaga@users.noreply.github.com>
2020-08-28 17:44:23 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,7 +21,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :text, :category_id, :condition_id, :shipping_origin_id, :delivery_fee_id, :delivery_day_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :text, :category_id, :condition_id, :shipping_origin_id, :delivery_fee_id, :delivery_day_id, :price, :image).merge(user_id: current_user.id)
   end
 
   def move_to_index
